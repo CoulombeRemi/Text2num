17cfbe5c809b0cc8a72eb3a368846b40a3be13c2
Unknown <rem.coulombe@gmail.com>
2019-10-27 14:48:59 -0400

    play sound array | random speed

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from pyo import *
 import os
+import random
 s = Server().boot()
 s.start()
 
 
 alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",",",".","\n"]
 file= open("textfile.txt","r")
-sFolder = "sounds/"
-#sounds = ["00.wav","01.wav","02.wav","03.wav","04.wav","05.wav","06.wav","7.wav","08.wav","09.wav","10.wav","11.wav","12.wav","13.wav","14.wav","15.wav","16.wav","17.wav","18.wav","19.wav","20.wav","21.wav","22.wav","23.wav","24.wav","25.wav","26.wav","27.wav","28.wav","30.wav"]
+sPath = "sounds/"
 sounds = []
 soundsListLen = list()
+readSpeed = random.uniform(.1,1)
 
 # Convert letters array to num
 def let2num(letter):
@@ -18,22 +19,22 @@ x = map(let2num, file.read())
 
 
 # Get nbr of files in directory
-dirs = os.listdir(sFolder)
+dirs = os.listdir(sPath)
 for file in dirs:
     soundsListLen.append(file)
 listLength = len(soundsListLen)
-#print(listLength)
 
 # Put files in direct. into sounds array
 for i in range(listLength):
     sounds.append(dirs[i])
-print(sounds)
-
 
 # fonction calling each sounds in for loop
 def loopSound():
     for i in x:
-        player = SfPlayer(sFolder+sounds[i], speed=1, mul=0.5).mix(2).out()
+        player = SfPlayer(sPath+sounds[i], speed=readSpeed, mul=0.5).mix(2).out()
+        
+
+
 
 loopSound()
 
