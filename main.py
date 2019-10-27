db366f6b9bf3a6a315f68ab36194e85999a4611f
Rémi Coulombe <rem.coulombe@gmail.com>
2019-10-27 17:23:40 -0400

    array as to be the same len. as the nbr of sounds

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import os
 import random
 s = Server().boot()
 s.start()
+#s.amp = .3
 
-
+# 48
 alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"," ",",",".","\n","-","$","@","!","\'","\"","(",")","0","1","2","3","4","5","6","7","8","9"]
 file= open("textfile.txt","r")
 sPath = "sounds/"
 sounds = []
 soundsListLen = list()
 readSpeed = random.uniform(.75,1.1)
+
+
 
 # Convert letters array to num
 def let2num(letter):
@@ -28,19 +31,14 @@ listLength = len(soundsListLen)
 for i in range(listLength):
     sounds.append(dirs[i])
 
+print(len(alpha))
+print(len(sounds))
 # fonction calling each sounds in for loop
 def loopSound():
     for i in x:
         player = SfPlayer(sPath+sounds[i], speed=readSpeed, mul=0.5).mix(2).out()
         
-
-
-
 loopSound()
-
-
-
-
 
 
 print("end")
